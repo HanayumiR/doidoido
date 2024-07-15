d959ec7033049819bf3d5f9121e299feb7e4f6ca
けゅ。 <54940894+HanayumiR@users.noreply.github.com>
2024-07-15 14:54:16 +0900

    祝日だよ
    祝日APIを使って明日が祝日かどうかを分岐で判断させた

--- bot.py
+++ bot.py
@@ -1,20 +1,23 @@
+#いろいろ取得したりするやつ
 import discord
 from discord.ext import commands
 import asyncio
 import os
 import json
 from datetime import datetime, timedelta
+import requests
 
-TOKEN = 'TOKEN_ID'#この中にトークンを追加
+TOKEN = 'YOUR_DISCORD_TOKEN'
 CHANNEL_ID_FILE = 'channel_id.json'
+HOLIDAYS_API_URL = 'https://holidays-jp.github.io/api/v1/date.json'
 
-# 接続処理
+#接続処理
 intents = discord.Intents.default()
-intents.message_content = True 
+intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-channel_id = None  # メッセージを送信するチャンネルIDを保存するための変数
-reminder_task = None  # リマインダーのタスクを保存する変数
+channel_id = None  #メッセージ送信チャンネルID用保存変数
+reminder_task = None  #タスク用保存変数
 
 def load_channel_id():
     global channel_id
@@ -28,6 +31,18 @@ def save_channel_id():
     with open(CHANNEL_ID_FILE, 'w') as file:
         json.dump({'channel_id': channel_id}, file)
 
+def get_holidays():
+    response = requests.get(HOLIDAYS_API_URL)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {}
+
+holidays = get_holidays()
+
+def is_holiday(date):
+    return date.strftime('%Y-%m-%d') in holidays
+
 @bot.event
 async def on_ready():
     print('ログインしました！')
@@ -35,7 +50,7 @@ async def on_ready():
     print('スラッシュコマンドが同期されました')
     load_channel_id()
     if channel_id is not None:
-        print(f'リマインダー設定が読み込まれたよ。: {channel_id}')
+        print(f'チャンネル通知設定が読み込まれましたよ。: {channel_id}')
         global reminder_task
         reminder_task = bot.loop.create_task(send_reminder())
     
@@ -52,26 +67,33 @@ async def doidoido(interaction: discord.Interaction):
     if reminder_task is None or reminder_task.done():
         reminder_task = bot.loop.create_task(send_reminder())
 
-# 指定した日時にメッセージを送信する関数
+#指定日時メッセージ送信関数
 async def send_reminder():
     global channel_id
     while True:
         if channel_id is not None:
             channel = bot.get_channel(channel_id)
             if channel:
-                # 日曜日の19:00を計算する
+                #日曜日の19:00をカウント
                 now = datetime.now()
                 target_time = now.replace(hour=19, minute=0, second=0, microsecond=0)
                 if now.weekday() == 6 and now >= target_time:
-                    # 日曜日の19:00以降の場合、次の週の日曜日19:00まで待つ
+                    #現時刻が日曜日の19:00以降の場合は次週まで待機
                     target_time += timedelta(days=7)
                 delta = target_time - now
                 await asyncio.sleep(delta.total_seconds())
-                await channel.send('月曜が近いです！')
-                await channel.send('https://video.twimg.com/ext_tw_video/1779366668697055233/pu/vid/avc1/1280x720/tIK_0IgHkJNaL5Qf.mp4')
-        await asyncio.sleep(3600)  # 1時間ごとにチェック
+                
+                #明日が祝日の場合
+                if is_holiday(target_time + timedelta(days=1)):
+                    await channel.send('明日は祝日です！')
+                    await channel.send('https://video.twimg.com/ext_tw_video/1812141628598915072/pu/vid/avc1/1280x720/NWGNABfw5uo3fazF.mp4')
+                #そうではない場合
+                else:
+                    await channel.send('月曜が近いです！')
+                    await channel.send('https://video.twimg.com/ext_tw_video/1779366668697055233/pu/vid/avc1/1280x720/tIK_0IgHkJNaL5Qf.mp4')
+        await asyncio.sleep(3600)  #1時間ごとにチェック
 
-# 返信処理
+#返信処理
 @bot.event
 async def on_message(message):
     if message.author.bot:
